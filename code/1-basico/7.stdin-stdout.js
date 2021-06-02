6cd752aca49209418e85bafc699dcfad6d596ed4
Andres Giraldo <andresganc@gmail.com>
2021-06-02 17:35:10 -0500

    New: STDIN - STDOUT

---
+++ code/1-basico/7.stdin-stdout.js
@@ -0,0 +1,9 @@
+
+// STDOUT
+// Es el comando para dar salida de datos por consola en NodeJS
+
+process.stdout.write('USE STDOUT NODEJS with jump line \n')
+
+process.stdout.write('USE STDOUT NODEJS without jump line')
+
+
